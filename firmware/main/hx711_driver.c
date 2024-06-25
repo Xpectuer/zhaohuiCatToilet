f923f3f88babd8d70f479d70b1ab4b979b068ded
Cheshire cat <shsty@sina.com>
2024-06-25 22:04:48 +0800

    fix bugs for hx711 driver

--- firmware/main/hx711_driver.c
+++ firmware/main/hx711_driver.c
@@ -16,7 +16,7 @@
 #define MAX(a,b) (((a)>(b)) ? (a) : (b))
 
 static const char *TAG = "weight";
-static const UBaseType_t dio_notify_index = 1;
+static const UBaseType_t do_notify_index = 1;
 
 static bool HX711_initialized = false;
 static TaskHandle_t HX711_task_handle = NULL;
@@ -25,7 +25,7 @@ static long HX711_data = 0;
 void HX711_DO_isr(void *pvParameters){
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
     assert(HX711_task_handle != NULL);
-    vTaskNotifyGiveIndexedFromISR(HX711_task_handle, dio_notify_index, &xHigherPriorityTaskWoken);
+    vTaskNotifyGiveIndexedFromISR(HX711_task_handle, do_notify_index, &xHigherPriorityTaskWoken);
     // request a context switch if a higher priority task is woken
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
@@ -49,17 +49,19 @@ esp_err_t HX711_init(){
 	io_conf.pull_up_en = 0;
 	ESP_RETURN_ON_ERROR(gpio_config(&io_conf), TAG, "configure GPIO failed for DO");
 
-    //install interrupt callback
+    //install callback service for all GPIOs
     esp_err_t err_code = gpio_install_isr_service(ESP_INTR_FLAG_LOWMED);
     if (err_code == ESP_ERR_INVALID_STATE){
-        ESP_LOGI(TAG, "GPIO ISR service already installed");
+        ESP_LOGW(TAG, "GPIO ISR service already installed");
     }
     else {
         ESP_RETURN_ON_ERROR(err_code, TAG, "install GPIO ISR service GPIO failed");
     }
 
+    //install interrupt callback
 	ESP_RETURN_ON_ERROR(gpio_isr_handler_add(ADDO_GPIO, HX711_DO_isr, NULL), TAG, "configure GPIO failed for DO");
 
+    //set CLK=0
     gpio_set_level(ADSK_GPIO, 0);
 
     HX711_initialized = true;
@@ -74,21 +76,19 @@ long HX711_read(int next_sense){
     long count;
     unsigned char i;
 
-    count=0;
-    while(gpio_get_level(ADDO_GPIO));
-    for (i=0;i<24;i++)
-    {
+    count = 0;
+    for (i = 0; i < 24; i++) {
         gpio_set_level(ADSK_GPIO, 1);
-        count = count<<1; 
+        count = count << 1; 
         gpio_set_level(ADSK_GPIO, 0); 
-        if(gpio_get_level(ADDO_GPIO)) count++;
+        if (gpio_get_level(ADDO_GPIO) == 1) 
+            count++;
     }
 
     //setting transformation mode for next data
-    for (i=0;i<24;i++)
-    {
-    gpio_set_level(ADSK_GPIO, 1);
-    gpio_set_level(ADSK_GPIO, 0);
+    for (i = 0; i < next_sense; i++) {
+        gpio_set_level(ADSK_GPIO, 1);
+        gpio_set_level(ADSK_GPIO, 0);
     }
 
     //handle 2's compliment for 24 bit input
@@ -120,14 +120,16 @@ void HX711_task(void *pvParameters){
     while (1) {
         //wait for notification from ISR if not ready
         if (gpio_get_level(ADDO_GPIO) == 1)
-            ulTaskNotifyTakeIndexed(dio_notify_index, pdTRUE, portMAX_DELAY);
+            ulTaskNotifyTakeIndexed(do_notify_index, pdTRUE, portMAX_DELAY);
         //enable interrupt on DO
         gpio_intr_disable(ADDO_GPIO);
         //read data from HX711
         HX711_data = HX711_read(CHANNEL_A_x128);
         //enable interrupt on DO
         gpio_intr_enable(ADDO_GPIO);
-        printf("%s: %lx", TAG, HX711_data);
+        printf("%s: %ld\n", TAG, HX711_data);
+        //delay 50 ms to prevent spamming output
+        vTaskDelay(50 / portTICK_PERIOD_MS);
     }
 }
 
